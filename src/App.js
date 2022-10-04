b19dfc4780e41d8e01d835762a8659ae7e85a307
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-10-04 16:30:59 +0200

    registration form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import { useState } from 'react';
 import './App.css';
+import { v4 as uuidv4 } from "uuid";
+
  
 function App () {
 const[name,setName]=useState("");
 const[surname,setSurname]=useState("");
 const[email,setEmail]=useState("");
-const[id,setId]=useState("");
+const[idNumber,setIdNumber]=useState("");
 const[phone,setPhone]=useState("");
 const[table,setTable]=useState([]);
+const [list, setList] = useState([]);
+
+const handleDelete = (id) => {
+  console.log(id)
+    let newlist = table.filter((item) => item.id !== id);
+    setTable([...newlist]);
+  };
 
 const handleSubmit=(e)=>{
-  console.log(name,surname,email,id,phone)
-  const date={name,surname,email,id,phone}
-  if(name&&surname&&email&&id&&phone){
+  e.preventDefault()
+  list.id = uuidv4();
+  console.log(name,surname,email,idNumber,phone)
+  const date={id:uuidv4(),name,surname,email,idNumber,phone}
+  if(name&&surname&&email&&idNumber&&phone){
     setTable((th)=>[...th,date])
     setName("")
     setSurname("")
     setEmail("")
-    setId("")
+    setIdNumber("")
     setPhone("")
-  }
+  };
+
+  
 }
   return(
-    <div className="App">
-    <h1>React</h1>
+    <div className="register">
+    <h1>Register</h1>
     <form onSubmit={handleSubmit}>
 
     <div>
@@ -43,7 +56,7 @@ const handleSubmit=(e)=>{
 
     <div>
     <div type="label">Id Number:</div>
-    <input name="id" placeholder='id' value={id} onChange={(e)=>setId(e.target.value)}/>
+    <input name="idNumber" placeholder='id' value={idNumber} onChange={(e)=>setIdNumber(e.target.value)}/>
     </div>
 
     <div>
@@ -51,18 +64,39 @@ const handleSubmit=(e)=>{
     <input name="phone" placeholder='phone' value={phone} onChange={(e)=>setPhone(e.target.value)}/>
     </div>
 
-    <button>Submit</button>
+    <button id="submit">Submit</button>
     </form>
+    <br/>
+    <div className='table-container'>
+    <table>
+    <thead>
+    <tr>
+    <th>Name</th>
+    <th>Surname</th>
+    <th>Email</th>
+    <th>Id</th>
+    <th>Phone</th>
+    <th>Delete</th>
+    </tr>
+    </thead>
+    <tbody>
+    {
+      table.map((item,index)=><tr key={index}>
+       <td>{item.name}</td>
+       <td>{item.surname}</td>
+       <td>{item.email}</td>
+       <td>{item.idNumber}</td>
+       <td>{item.phone}</td>
+       <td> <button onClick={() => handleDelete(item.id)}>
+       Delete
+     </button></td>
+      </tr>)
+    }
+    </tbody>
+    </table>
+    </div>
 
-{
-  table.map((a)=><div>
-   <th>{a.name}</th>
-   <th>{a.surname}</th>
-   <th>{a.email}</th>
-   <th>{a.id}</th>
-   <th>{a.phone}</th>
-  </div>)
-}
+
 
     </div>
   );
